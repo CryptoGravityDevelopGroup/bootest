cadbd83c8ca91d3019bb4eda820fd64a904d7d09
zhaobowen <1964471757@qq.com>
2022-07-31 00:36:25 +0800

    fixfix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from "react";
 import './App.css';
+import Main from "./modules/main/index.tsx";
+import Inituser from "./modules/inituser/index.tsx";
+import NftDetail from "./modules/nftDetail/index.tsx";
+import Profile from "./modules/profile/index.tsx";
+import TokenDetail from "./modules/tokenDetail/index.tsx";
 
 function App() {
+  console.log('2.54')
+  // 自适应
+  useEffect(() => {
+    window.onload = function () {
+        getRem(1440, 1)
+    };
+    window.onresize = function () {
+        getRem(1440, 1)
+    };
+    
+    function getRem(pwidth, prem) {
+        const html = document.getElementsByTagName("html")[0];
+        const oWidth = document.body.clientWidth || document.documentElement.clientWidth;
+        html.style.fontSize = oWidth / pwidth * prem + "px";
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/main" element={<Main/>}/>
+        <Route path="/inituser" element={<Inituser/>}/>
+        <Route path="/nftDetial" element={<NftDetail/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/tokenDetail" element={<TokenDetail/>}/>
+        <Route path="*" element={<Navigate to="/main"/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
